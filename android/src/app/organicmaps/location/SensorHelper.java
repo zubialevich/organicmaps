70c1fc159cc0f4042d3ccdf7a92e6d35ed8b15e8
Alexander Borsuk <me@alex.bio>
2023-05-11 13:50:26 +0200

    [android] Fixed device orientation/compass on some devices without gyroscope
    For example, Samsung SM-C7000
    
    Signed-off-by: Alexander Borsuk <me@alex.bio>

--- android/src/app/organicmaps/location/SensorHelper.java
+++ android/src/app/organicmaps/location/SensorHelper.java
@@ -16,81 +16,80 @@ import app.organicmaps.R;
 
 class SensorHelper implements SensorEventListener
 {
-  @Nullable
+  @NonNull
   private final SensorManager mSensorManager;
   @Nullable
-  private Sensor mRotation;
+  private final Sensor mRotationVectorSensor;
   @NonNull
   private final MwmApplication mMwmApplication;
+
+  private final float[] mRotationMatrix = new float[9];
+  private final float[] mRotationValues = new float[3];
 
   @Override
   public void onSensorChanged(SensorEvent event)
   {
-    if (!mMwmApplication.arePlatformAndCoreInitialized())
-      return;
+    // Here we can have events from one out of these two sensors:
+    // TYPE_GEOMAGNETIC_ROTATION_VECTOR
+    // TYPE_ROTATION_VECTOR
+    SensorManager.getRotationMatrixFromVector(mRotationMatrix, event.values);
+    SensorManager.remapCoordinateSystem(mRotationMatrix, SensorManager.AXIS_X, SensorManager.AXIS_Y, mRotationMatrix);
 
-    notifyInternal(event);
-  }
+    SensorManager.getOrientation(mRotationMatrix, mRotationValues);
 
-  private final static int kRotationVectorSensorType = Sensor.TYPE_ROTATION_VECTOR;
-  private final float[] mRotationMatrix = new float[9];
-  private final float[] mRotationValues = new float[3];
-
-  private void notifyInternal(@NonNull SensorEvent event)
-  {
-    if (event.sensor.getType() == kRotationVectorSensorType)
-    {
-      SensorManager.getRotationMatrixFromVector(mRotationMatrix, event.values);
-      SensorManager.remapCoordinateSystem(mRotationMatrix, SensorManager.AXIS_X, SensorManager.AXIS_Y, mRotationMatrix);
-
-      SensorManager.getOrientation(mRotationMatrix, mRotationValues);
-
-      // mRotationValues indexes: 0 - yaw, 2 - roll, 1 - pitch.
-      LocationHelper.INSTANCE.notifyCompassUpdated(mRotationValues[0]);
-    }
+    // mRotationValues indexes: 0 - yaw, 2 - roll, 1 - pitch.
+    LocationHelper.INSTANCE.notifyCompassUpdated(mRotationValues[0]);
   }
 
   @Override
   public void onAccuracyChanged(Sensor sensor, int accuracy)
   {
+    // Here we can have events from one out of these two sensors:
+    // TYPE_GEOMAGNETIC_ROTATION_VECTOR
+    // TYPE_ROTATION_VECTOR
     Log.i("onAccuracyChanged", "Sensor " + sensor.getStringType() + " has changed accuracy to " + accuracy);
-    if (sensor.getType() == kRotationVectorSensorType)
+    String toastText = null;
+    switch (accuracy)
     {
-      String toastText = null;
-      switch (accuracy)
-      {
-        case SensorManager.SENSOR_STATUS_ACCURACY_HIGH:
-          break;
-        case SensorManager.SENSOR_STATUS_ACCURACY_MEDIUM:
-          toastText = mMwmApplication.getString(R.string.compass_calibration_recommended);
-          break;
-        case SensorManager.SENSOR_STATUS_ACCURACY_LOW:
-        case SensorManager.SENSOR_STATUS_UNRELIABLE:
-        default:
-          toastText = mMwmApplication.getString(R.string.compass_calibration_required);
-      }
-      if (toastText != null)
-        Toast.makeText(mMwmApplication, toastText, Toast.LENGTH_LONG).show();
+      case SensorManager.SENSOR_STATUS_ACCURACY_HIGH:
+        break;
+      case SensorManager.SENSOR_STATUS_ACCURACY_MEDIUM:
+        toastText = mMwmApplication.getString(R.string.compass_calibration_recommended);
+        break;
+      case SensorManager.SENSOR_STATUS_ACCURACY_LOW:
+      case SensorManager.SENSOR_STATUS_UNRELIABLE:
+      default:
+        toastText = mMwmApplication.getString(R.string.compass_calibration_required);
     }
+    if (toastText != null)
+      Toast.makeText(mMwmApplication, toastText, Toast.LENGTH_LONG).show();
   }
 
   SensorHelper(@NonNull Context context)
   {
     mMwmApplication = MwmApplication.from(context);
     mSensorManager = (SensorManager) mMwmApplication.getSystemService(Context.SENSOR_SERVICE);
-    if (mSensorManager != null)
-      mRotation = mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+    Sensor sensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+    if (sensor == null)
+    {
+      Log.w("SensorHelper", "WARNING: There is no ROTATION_VECTOR sensor, requesting GEOMAGNETIC_ROTATION_VECTOR");
+      sensor = mSensorManager.getDefaultSensor(Sensor.TYPE_GEOMAGNETIC_ROTATION_VECTOR);
+      if (sensor == null)
+        Log.w("SensorHelper", "WARNING: There is no GEOMAGNETIC_ROTATION_VECTOR sensor, device orientation can not be calculated");
+    }
+    // Can be null in rare cases on devices without magnetic sensors.
+    mRotationVectorSensor = sensor;
   }
 
   void start()
   {
-    if (mRotation != null && mSensorManager != null)
-      mSensorManager.registerListener(this, mRotation, SensorManager.SENSOR_DELAY_UI);
+    if (mRotationVectorSensor != null)
+      mSensorManager.registerListener(this, mRotationVectorSensor, SensorManager.SENSOR_DELAY_UI);
   }
 
   void stop()
   {
-    if (mSensorManager != null)
+    if (mRotationVectorSensor != null)
       mSensorManager.unregisterListener(this);
   }
 }
